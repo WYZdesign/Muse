b6f20add17352875ebf9b16b8914a557c1fac4e2
WYZ Design <torree.marcel@gmail.com>
2026-08-06 20:56:30 -0700

    chore: add eslint flat config for eslint 9 compat
    eslint-config-next 16.2.6 has circular ref bug with eslint 9 flat
    config; use minimal config with ignores for .next/node_modules

---
+++ eslint.config.js
@@ -0,0 +1,10 @@
+export default [
+  {
+    ignores: [".next/**", "node_modules/**", "out/**", "build/**", "*.config.js", "*.config.mjs"],
+  },
+  {
+    rules: {
+      "no-unused-vars": "warn",
+    },
+  },
+];
